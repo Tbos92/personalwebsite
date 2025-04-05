9328477645c84762a670105a6f3729386e592944
Tyler <tbos92@gmail.com>
2025-04-04 22:13:32 -0400

    Added basic footer with offCanvas component to expand contact form from bottom of screen in addition to the contact route. Also added functional links to LinkedIn and Github via icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,26 +12,24 @@ import BlogDetailView from "./components/BlogDetailView.jsx";
 import Contact from "./components/Contact.jsx";
 import ContactForm from "./components/ContactForm.jsx";
 import PortfolioHighlights from "./components/PortfolioHighlights.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <div id="container">
-        <div id="navbar">
-          <Navbar />
-        </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog_detail_view" element={<BlogDetailView />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/highlights" element={<PortfolioHighlights />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog_detail_view" element={<BlogDetailView />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/highlights" element={<PortfolioHighlights />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
