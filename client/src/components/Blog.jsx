2725ed8fc35d462d84921d3167a870d94066377d
Tbos92 <tbos92@gmail.com>
2025-04-25 17:00:23 -0400

    Added template for blog cards

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,5 +1,19 @@
+import BlogCard from "./BlogCard";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
 const Blog = () => {
-  return <h1>Blog Page</h1>;
+  return (
+    <Container>
+      <h1>Blog Page</h1>
+      <Row className="justify-content-center">
+        <Col xs={11} lg={6}>
+          <BlogCard />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Blog;
